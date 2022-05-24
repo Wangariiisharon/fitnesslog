e93b15aa78aa1e4824da37c1d6e672cf7cf5086a
Wangariiisharon <mwangiiisharon@gmail.com>
2022-05-24 19:16:33 +0300

    Fitness commit

--- app/src/main/java/dev/mwangi/fitnesslog/SplashActivity.kt
+++ app/src/main/java/dev/mwangi/fitnesslog/SplashActivity.kt
@@ -3,9 +3,9 @@ package dev.mwangi.fitnesslog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
